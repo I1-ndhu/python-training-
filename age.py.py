ba2f3bfb4640d2745a6b4cd1f3eca961eac67523
I1-ndhu <indhuthasakandan@gmail.com>
2025-09-24 21:34:49 +0530

    24/9/2025

---
+++ age.py.py
@@ -0,0 +1,9 @@
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+
+# Perform a small calculation
+next_year_age = age + 1
+
+# Display result
+print(f"Hi {name}, you are {age} years old.")
+print(f"Next year, you will be {next_year_age} years old!")
